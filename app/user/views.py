289d1b37ad06c258176fe54b6c3fe7557ec486a9
developpex <developpex@gmail.com>
2025-05-21 18:46:13 +0700

    fix bugs in the user api

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 Views for the user API.
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -15,6 +15,17 @@ from user.serializers import (
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+
+class ManageUserViews(generics.RetrieveUpdateAPIView):
+    """Mange the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
 
 
 class CreateTokenView(ObtainAuthToken):
