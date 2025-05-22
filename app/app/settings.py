5a4b1875ab356e3cb74536d28528af516b981989
developpex <developpex@gmail.com>
2025-05-22 14:29:17 +0700

    Add recipe api

--- app/app/settings.py
+++ app/app/settings.py
@@ -17,7 +17,6 @@ from django.conf.global_settings import AUTH_USER_MODEL
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = 'django-insecure-7s&xmcj+@pqv84$_os&)928)+gn&t2%4g%bhould*$%9f(ng==
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -41,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'user',
+    'recipe',
     'rest_framework',
     'rest_framework.authtoken',
     'drf_spectacular'
@@ -76,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -89,7 +87,6 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -109,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -122,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,17 @@
 """
 Test for models.
 """
+from decimal import Decimal
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
+
+
+def create_user(email="user@example.com", password="testpass123"):
+    """Create a user for the tests"""
+    return get_user_model().objects.create_user(email=email, password=password)
 
 
 class ModelTests(TestCase):
@@ -50,3 +58,20 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "testpassword123"
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Test Name",
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Test description"
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
